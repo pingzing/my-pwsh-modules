f46029e9470bad2d9eac6852888531fbc43490f5
Neil McAlister <neil.mcalister@f-secure.com>
2022-03-22 15:05:51 +0200

    Add note to edit-sharedscripts

--- my-functions.psm1
+++ my-functions.psm1
@@ -34,6 +34,7 @@ function Edit-Profile {
     code (Get-Item $Profile).Directory.FullName;
 }
 
+# Relies on on the $SharedWslDir environment variable to already exist. It should be created manually
 function Edit-SharedScripts {
     code "$env:SharedWslDir/.powershell/modules"
 }
