0cfe0c6f2d6b0e9f33d702d7dcdc2f18c396f601
pingzing <mcalistern@gmail.com>
2024-05-19 13:55:25 +0300

    Add az CLI completions

--- my-functions.psm1
+++ my-functions.psm1
@@ -1,3 +1,6 @@
+#Some future todo:
+# Make this only export the functions I actually want to be public, instead of literally every function in the whole module
+
 #Import child .ps1 files and dot-source them
 $pathFunctions = @(Get-ChildItem -Path $PSScriptRoot/path/*.ps1);
 $gitFunctions = @(Get-ChildItem -Path $PSScriptRoot/git/*.ps1);
@@ -7,15 +10,17 @@ $dotnetFunctions = @(Get-ChildItem -Path $PSScriptRoot/dotnet/*.ps1);
 $workFunctions = @(Get-ChildItem -Path $PSScriptRoot/work/*.ps1);
 $shellFunctions = @(Get-ChildItem -Path $PSScriptRoot/shell/*.ps1);
 $stringsFunctions = @(Get-ChildItem -Path $PSScriptRoot/strings/*.ps1);
+$azureFunctions = @(Get-ChildItem -Path $PSScriptRoot/azure/*.ps1);
 foreach ($child in 
-            $pathFunctions + 
-            $gitFunctions + 
-            $rustupFunctions + 
-            $networkFunctions + 
-            $dotnetFunctions +
-            $workFunctions +
-            $shellFunctions + 
-            $stringsFunctions) {
+    $pathFunctions + 
+    $gitFunctions + 
+    $rustupFunctions + 
+    $networkFunctions + 
+    $dotnetFunctions +
+    $workFunctions +
+    $shellFunctions + 
+    $stringsFunctions +
+    $azureFunctions) {
     try {
         . $child.fullname
     }
@@ -24,14 +29,19 @@ foreach ($child in
     }
 }
 
-function Start-Csi([string]$file) {
+function Start-Csi([string]$file = $null) {
     $vswherePaths = & "${Env:ProgramFiles(x86)}\Microsoft Visual Studio\Installer\vswhere.exe" -latest -requires Microsoft.Component.MSBuild -find MSBuild\**\csi.exe;
     if (-Not($vswherePaths)) {
         throw "Unable to find any installed csi.exe.";
     }
 
     $csiExePath = $vswherePaths | Select-Object -First 1;
-    & $csiExePath $file;
+    if ($file) {
+        & $csiExePath $file;
+    }
+    else {
+        & $csiExePath;
+    }
 }
 
 function Edit-Profile {
